8265d1ff7ff75a6f2eb724922303a9895a6672ae
Jordan Triplett <JordanT111596@gmail.com>
2020-09-01 21:29:21 -0400

    Added code for showing correct pages by url, started app gets, posts, and listen

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const dataBase = require("./db/db.json")
 
 // Sets up the Express App
 // =============================================================
@@ -13,30 +14,41 @@ const PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Basic route that sends the user to the index page
-app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
-});
-
 // Basic route that sends the user to the notes page
 app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "./public/notes.html"));
+    res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
+
+// Basic route that sends the user to the index page
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
 // API route that reads the db.json file and returns saved notes as JSON
 app.get("/api/notes", function (req, res) {
-    fs.readFile("./db/db.json", function (err, data) {
+    fs.readFile(dataBase, "utf8", function (err, data) {
         if (err) throw err;
+        data = JSON.parse(data);
         return res.json(data);
     });
 });
 
 // Create New Notes - takes in JSON input
-app.post("/api/notes", function(req, res) {
+app.post("/api/notes", function (req, res) {
     var newNote = req.body;
+    newNote = JSON.stringify(newNote);
+    var dataBase = JSON.parse(dataBase);
+    console.log(dataBase);
+    console.log(newNote);
     //pushes the new note into the db.json file
-    //(db.json).push(newNote);
-    res.json(newNote);
+    dataBase.push(newNote);
+    // writes the new note to file
+    fs.writeFile("./Develop/db/db.json", dataBase, "utf8", function (err) {
+        // error handling
+        if (err) throw err;
+    });
+    // changes dataBase back to an array of objects & sends it back to the client
+    res.json(JSON.parse(dataBase));
 });
 
 // Delete saved notes
@@ -44,7 +56,6 @@ app.post("/api/notes", function(req, res) {
 
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
-  });
-  
+});
